9f5043c3186fa8e6e021ca55a7d4520a0071caf9
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-06-16 14:28:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// your code here
+function handleclick() {
+                const name = document.getElementById("name").value;
+                const year = document.getElementById("year").value;
+                const outputUrl = `https://localhost:8080/?name=${name}&year=${year}`;
+                document.getElementById("output-url").innerText = outputUrl;
+            }
